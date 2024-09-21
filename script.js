bed0df0889657569d97f9f868f292ed9daae529a
Cris Sousa <59988469+kneeshe@users.noreply.github.com>
2024-09-21 18:35:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+function calculadora(){ //função para calcular a potencia do valor que o usuario inserir
+    let pot = document.getElementById('val').value; //variavel para pegar o valor do input do usuario
+    let res = pot ** 2; //calcula a exponenciação sobre 2
+    document.getElementById('showResult').innerHTML = res; //localiza o elemento pelo id e mostra o resultado da variavel na div
+}
